9189b1c86d17c504f2648cf7b04cd6e8ec90d799
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2025-11-02 00:59:24 -0400

    Update fai-ubuntu-setup.sh

--- fai-ubuntu-setup.sh
+++ fai-ubuntu-setup.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-
-# Minimal version - save as minimal_setup.sh
-
-echo "Starting minimal setup..."
-
-# Create black wallpaper
-convert -size 1024x768 xc:black "$HOME/black.png"
-
-# Set black background (GNOME)
-gsettings set org.gnome.desktop.background picture-uri "file://$HOME/black.png"
-gsettings set org.gnome.desktop.screensaver picture-uri "file://$HOME/black.png"
-
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
-
-# Create desktop folders
-mkdir -p "$HOME/Desktop/Docker-Projects"
-mkdir -p "$HOME/Desktop/Config-Files"
-
-echo "Minimal setup complete! Logout required for Docker."
